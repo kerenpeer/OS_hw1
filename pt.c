ca0942470cb9f7ac2f508d306eb903d79a6afe05
kerenpeer <kerenpeer97@gmail.com>
2021-11-03 17:20:12 +0200

    Update pt.c

--- pt.c
+++ pt.c
@@ -14,8 +14,8 @@ void page_table_update(uint64_t pt, uint64_t vpn, uint64_t ppn);
 uint64_t page_table_query(uint64_t pt, uint64_t vpn);
 
 int get_curr(uint64_t vpn, int j){
-    uint64_t curr, shift;
-    int mask;
+    uint64_t shift;
+    int curr, mask;
     mask = ((1 << VPN_BLOCK_SIZE) - 1);
     shift = (vpn >> (45-9*j));
     curr = shift & mask;
