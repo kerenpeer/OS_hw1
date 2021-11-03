914cbaed1ae3286209cc9c3df64582e9941f7f56
kerenpeer <kerenpeer97@gmail.com>
2021-11-03 14:54:47 +0200

    Update pt.c

--- pt.c
+++ pt.c
@@ -1,5 +1,4 @@
 # include "os.h"
-#include <inttypes.h>
 # define LEVELS 5
 # define VPN_BLOCK_SIZE 9
 
@@ -14,7 +13,6 @@ uint64_t get_curr(uint64_t vpn, int j){
     int mask;
     mask = ((1 << VPN_BLOCK_SIZE) - 1);
     curr = (vpn >> (45-9*j)) & mask;
-    printf("%" PRIu64 "\n", curr);
     return curr; 
 }
 
@@ -48,9 +46,9 @@ void page_table_update(uint64_t pt, uint64_t vpn, uint64_t ppn){
 
 uint64_t page_table_query(uint64_t pt, uint64_t vpn){
     int i;
-    uint64_t curr_index, pte, ppn;
+    uint64_t curr_index, pte, ppn, last_index;
 
-    uint64_t * level = phys_to_virt(pt << 12);
+    uint64_t* level = phys_to_virt(pt << 12);
 
     for(i = 0; i < LEVELS; i++){
         curr_index = get_curr(vpn,i);
@@ -60,7 +58,8 @@ uint64_t page_table_query(uint64_t pt, uint64_t vpn){
         }
         level = phys_to_virt(pte & (~1));
     }
-    ppn = *level;
+    last_index = get_curr(vpn,5);
+    ppn = level[last_index];
     if((ppn & 1) != 1){
             return NO_MAPPING;
     }
