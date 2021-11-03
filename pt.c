de428f07b0fcfbb440af47ad351fe0e7265a9dcf
kerenpeer <kerenpeer97@gmail.com>
2021-11-03 14:42:55 +0200

    Update pt.c

--- pt.c
+++ pt.c
@@ -2,8 +2,13 @@
 # define LEVELS 5
 # define VPN_BLOCK_SIZE 9
 
+uint64_t get_curr(uint64_t vpn, int j);
+void page_table_update(uint64_t pt, uint64_t vpn, uint64_t ppn);
+uint64_t page_table_query(uint64_t pt, uint64_t vpn);
+
 uint64_t get_curr(uint64_t vpn, int j){
-    uint64_t curr, mask;
+    uint64_t curr;
+    int mask;
     mask = ((1 << VPN_BLOCK_SIZE) - 1);
     curr = (vpn >> (45-9*j)) & mask;
     return curr; 
@@ -11,13 +16,13 @@ uint64_t get_curr(uint64_t vpn, int j){
 
 void page_table_update(uint64_t pt, uint64_t vpn, uint64_t ppn){
     int i;
-    uint64_t curr_index, pte;
-    uint64_t * level = phys_to_virt(pt << 12);
-
+    uint64_t curr_index, pte, last_index;
+    
+    uint64_t* level = phys_to_virt(pt << 12);
     for(i = 0; i < LEVELS; i++){
         curr_index = get_curr(vpn,i);
         pte = level[curr_index];
-        if(pte == 0){
+        if((pte & 1) != 1){
             if(ppn == NO_MAPPING){
                 return;
             }
@@ -28,11 +33,12 @@ void page_table_update(uint64_t pt, uint64_t vpn, uint64_t ppn){
         }
         level = phys_to_virt(pte & (~1));
     }
+    last_index = get_curr(vpn,5);
     if(ppn == NO_MAPPING){
-        level[5] = 0;
+        level[last_index] = 0;
     }
     else{
-        level[5] = (ppn << 12)+1;
+        level[last_index]= (ppn << 12) | 1;
     }
 }
 
