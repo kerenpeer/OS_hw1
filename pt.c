c29939e40b28eb1d0036606f43ef5e06d933b4b2
kerenpeer <kerenpeer97@gmail.com>
2021-11-03 16:56:24 +0200

    Update pt.c

--- pt.c
+++ pt.c
@@ -55,7 +55,7 @@ uint64_t page_table_query(uint64_t pt, uint64_t vpn){
 
     uint64_t* level = phys_to_virt(pt << 12);
 
-    for(i = 1; i <=5 LEVELS; i++){
+    for(i = 1; i <= LEVELS; i++){
         curr_index = get_curr(vpn,i);
         pte = level[curr_index];
         if((pte & 1) != 1){
